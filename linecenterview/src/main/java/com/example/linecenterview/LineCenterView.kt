a11858e2a13922853454f54e6ba95edb7b93e601
Anwesh43 <anweshthecool0@gmail.com>
2021-06-12 11:11:41 +0530

    adding LCSNode class

--- linecenterview/src/main/java/com/example/linecenterview/LineCenterView.kt
+++ linecenterview/src/main/java/com/example/linecenterview/LineCenterView.kt
@@ -118,4 +118,45 @@ class LineCenterSquareView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LCSNode(val i : Int, private val state : State = State()) {
+
+        private var next : LCSNode? = null
+        private var prev : LCSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LCSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLCSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LCSNode {
+            var curr : LCSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
