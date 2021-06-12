03113dc9fbeeec9118fc74e1ce861fe1a2cd317d
Anwesh43 <anweshthecool0@gmail.com>
2021-06-12 11:04:24 +0530

    adding Canvas extension methods

--- linecenterview/src/main/java/com/example/linecenterview/LineCenterView.kt
+++ linecenterview/src/main/java/com/example/linecenterview/LineCenterView.kt
@@ -27,3 +27,31 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineCenterSquare(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..3) {
+        val y : Float = size * 0.5f * sc2
+        val upSize : Float = size * (1 - j % 2)  + size * (j % 2)* sc2
+        save()
+        translate((-w / 2 - size) * (1 - sc1), 0f)
+        rotate(90f * j)
+        drawLine(-upSize / 2 + size * sc3, y, upSize / 2, y, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawLCSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLCSNode(i, scale, paint)
+}
